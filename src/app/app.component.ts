6d907f0b8ca13fd2698ccaaf718978652c12ca1c
Maksym Polinka <Polinka7max@gmail.com>
2020-07-29 17:09:41 +0300

    went through the tutorial part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  // `]
 })
 export class AppComponent {
   name = 'Max';
+  username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails(): void {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
