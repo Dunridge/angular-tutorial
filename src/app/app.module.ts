f3bb8b87ff880b3f8e979fdc2274b2fb5e734a86
Maksym Polinka <Polinka7max@gmail.com>
2020-07-30 10:49:16 +0300

    went through the debugging lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,8 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
