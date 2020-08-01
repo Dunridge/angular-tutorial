578850d58d8a9fb4d1e66e3629696f9831e5125b
Maksym Polinka <Polinka7max@gmail.com>
2020-08-01 10:06:35 +0300

    created a custom hostlistener function

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,32 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     // you can add a flag for such styles as !important as a fourth parameter
   }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+
+  // test
+  @HostListener('click') mouseclick(eventData: Event) {
+    if (this.elRef.nativeElement.style.color === 'red') {
+      this.renderer.setStyle(this.elRef.nativeElement, 'color', 'darkblue');
+    } else {
+      this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red');
+    }
+  }
+  // -----------
+
 }
